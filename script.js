3d323497b9538b26490a614c8643747a3841aba4
Rakesh <ravig2006@gmail.com>
2023-10-15 00:51:17 +0530

    Adding files

---
+++ script.js
@@ -0,0 +1,20 @@
+function Filter(){
+    var input, filter, ul, li, a, i, txtValue;
+    input = document.getElementById('input');
+    filter = input.value.toUpperCase();
+    console.log(filter);
+    ul = document.getElementById('myUl');
+    li = document.getElementsByTagName('li');
+    console.log(li);
+
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName("a")[0];
+        txtValue = a.textContent || a.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+    }
+}
+
+}
